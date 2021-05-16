8408d81aca00e2050bea60c166d8768d7c2c4a19
patr-lew <patryk.a.lewandowski@gmail.com>
2021-05-16 16:40:59 +0200

    Initial commit

---
+++ src/main/java/com/lewandowski/wycena3000/Wycena3000Application.java
@@ -0,0 +1,13 @@
+package com.lewandowski.wycena3000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wycena3000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wycena3000Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lewandowski/wycena3000/Wycena3000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewandowski.wycena3000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wycena3000ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
