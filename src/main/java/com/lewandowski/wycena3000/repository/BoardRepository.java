9177e69ddd1a46bd27cb5af97e728fd42d5ca206
patr-lew <patryk.a.lewandowski@gmail.com>
2021-06-17 15:28:17 +0200

    updated new table names

--- src/main/java/com/lewandowski/wycena3000/repository/BoardRepository.java
+++ src/main/java/com/lewandowski/wycena3000/repository/BoardRepository.java
@@ -9,10 +9,10 @@ import java.util.Set;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    @Query(value = "select distinct b.* from board b join board_measurement bm on b.id = bm.board_id join project_board pb on bm.id = pb.board_id join project p on p.id = pb.project_id where p.id = ?1", nativeQuery = true)
+    @Query(value = "select distinct b.* from board b join measurement m on b.id = m.board_id join project_measurement pm on m.id = pm.measurement_id join project p on p.id = pm.project_id where p.id = ?1", nativeQuery = true)
     List<Board> findAllByProjectId(long projectId);
 
-    @Query(value = "select b.id from board_measurement bm right join board b on b.id = bm.board_id where bm.id is null", nativeQuery = true)
+    @Query(value = "select b.id from measurement m right join board b on b.id = m.board_id where m.id is null", nativeQuery = true)
     Set<Long> enabledDeleteSet();
 
     List<Board> findAllByUserIdOrderByIdAsc(long userId);
