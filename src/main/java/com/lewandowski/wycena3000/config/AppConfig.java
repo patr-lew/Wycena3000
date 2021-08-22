7644948576c4686ce12c85a5ed2199ec6bc05883
patr-lew <72469812+patr-lew@users.noreply.github.com>
2021-08-22 23:20:07 +0200

    testing internationalisation

--- src/main/java/com/lewandowski/wycena3000/config/AppConfig.java
+++ src/main/java/com/lewandowski/wycena3000/config/AppConfig.java
@@ -2,10 +2,16 @@ package com.lewandowski.wycena3000.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
@@ -15,6 +21,11 @@ public class AppConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("/index");
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver =
@@ -22,5 +33,19 @@ public class AppConfig implements WebMvcConfigurer {
         viewResolver.setPrefix("/templates/");
         viewResolver.setSuffix(".html");
         return viewResolver;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
     }
 }
