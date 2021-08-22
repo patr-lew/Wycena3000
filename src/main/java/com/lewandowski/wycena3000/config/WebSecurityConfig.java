5d0e878ce48786fc5808ef7afb1d446bb6dfc64d
patr-lew <patryk.a.lewandowski@gmail.com>
2021-08-22 23:44:24 +0200

    polishing

--- src/main/java/com/lewandowski/wycena3000/config/WebSecurityConfig.java
+++ src/main/java/com/lewandowski/wycena3000/config/WebSecurityConfig.java
@@ -25,9 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/")
                 .permitAll()
                 .and().httpBasic();
-//                .and()
-//                    .exceptionHandling()
-//                    .accessDeniedPage("error/403");
+
 
     }
 
